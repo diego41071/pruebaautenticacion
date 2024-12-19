bb0b3e6ca5fb97d6f6d77098c28e07ae2f50e0ff
diego41071 <alexander.diego.2007@hotmail.com>
2024-12-19 16:55:26 -0500

    estructura de proyecto

---
+++ PruebaAutenticacion.Net/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaAutenticacion.Net.Controllers
+{
+    [Route("api/authenticate")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
